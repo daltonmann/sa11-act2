26d3381acf643d4e05db14bb929a3ec4b56a5bf0
Dalton Mann <daltonjamann@gmail.com>
2024-03-21 13:09:23 -0500

    Completed problem 5

--- prob5.rb
+++ prob5.rb
@@ -1,8 +1,10 @@
 def parse_log_entry(log)
-  pattern = nil
+  pattern = /(INFO|WARN|ERROR): (.*)/
   match = log.match(pattern)
   [match[1], match[2]] if match
 end
 
 log_entry = "ERROR: Disk space low"
+log_entry2 = "INFO: Your disk usage is 98%"
 p parse_log_entry(log_entry).inspect
+p parse_log_entry(log_entry2).inspect
